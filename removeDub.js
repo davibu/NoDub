36d667868cca59b0499cca00d928bb65eddd76a4
davibu <9825602+davibu@users.noreply.github.com>
2026-01-03 23:44:46 +0100

    handle titles that only contains the language in braces without "dub"

--- removeDub.js
+++ removeDub.js
@@ -1,3 +1,31 @@
+function isTextDub(text){
+	if(text.includes("dub"))
+		return true;
+
+	// also check if language is in the title like "Anime Title (English)" or "Anime title (Español)"
+	if(text.includes("(") &&( text.endsWith(")") || text.endsWith(") "))){ // check if contains brackets like (some_text)
+		brace_text = text.substr(text.indexOf("("),text.length-text.indexOf("("));
+		brace_text = brace_text.trim();
+		brace_text = brace_text.toLowerCase();
+
+		let isLanguage = 
+		brace_text.includes("中文") ||
+		brace_text.includes("español") ||
+		brace_text.includes("português") ||
+		brace_text.includes("deutsch") ||
+		brace_text.includes("français") ||
+		brace_text.includes("italiano") ||
+		brace_text.includes("русский") ||
+		brace_text.includes("english");
+		
+		if(isLanguage)
+			return true;
+	}
+
+	return false;
+
+}
+
 function removeDubs(){
 	var allItems = document.getElementsByClassName("release")//document.getElementsByTagName("article");
 	var to_delete=[]
@@ -5,7 +33,7 @@ function removeDubs(){
 		var textElements = allItems[i].getElementsByTagName("cite");
 		for(let e of textElements){
 			text = e.innerHTML.toString().toLowerCase();
-			if( text.includes("dub)")){
+			if( isTextDub(text)){
 				console.log("REMOVING: "+text);
 				to_delete.push(allItems[i].parentElement);
 			}
